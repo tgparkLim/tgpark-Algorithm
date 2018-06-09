9bfab4e0c4e6dbece7fd726019f2e093c76aa5cd
park <sttgpark2@naver.com>
2018-06-09 21:11:45 +0900

    https://www.acmicpc.net/problem/1057

--- basic/src/Simulation/Main1057.java
+++ basic/src/Simulation/Main1057.java
@@ -12,32 +12,14 @@ public class Main1057 {
 		int N=Integer.parseInt(st.nextToken());
 		int A=Integer.parseInt(st.nextToken());
 		int B=Integer.parseInt(st.nextToken());
-
-		if(A>B){
-			int temp=A;
-			B=A;
-			A=temp;
-		}
 		
-		int round=1;
+		int round=0;
 		
-		if(N<A || N<B){
-			System.out.println(-1);
-		}else{
-			while(A!=B){
-				//System.out.println(A+" "+B);
-//				if(B%2==0 && A+1==B){
-//					break;
-//				}else{
-//					A=(A%2==0 ? A/2 : (A+1)/2);
-//					B=(B%2==0 ? B/2 : (B+1)/2);
-//				}
-//				round++;
-				A=(A%2==0 ? A/2 : (A+1)/2);
-				B=(B%2==0 ? B/2 : (B+1)/2);
-				round++;
-			}
-			System.out.println(round);
+		while(A!=B){
+			A=(A+1)/2;
+			B=(B+1)/2;
+			round++;
 		}
+		System.out.println(round);
 	}
 }
